2d37676aa14618813639f419e2adcb595dc73fc1
Hamid <HamidDoostparvar@gmail.com>
2021-12-02 19:57:05 +0330

    InitIchi

---
+++ StockMarketPrj/DW/Tables/FactIchi.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [DW].[FactIchi](
+	[NmdID] [int] NOT NULL,
+	[DteID] [int] NULL,
+	[Tenkan-sen] [int] NULL,
+	[Kijun-sen] [int] NULL,
+	[Senkou Span A] [int] NULL,
+	[Senkou Span B] [int] NULL,
+	[Chinkou Span] [int] NULL,
+	[ClosePrc] [decimal](26, 3) NULL
+) ON [PRIMARY]
+GO
+
